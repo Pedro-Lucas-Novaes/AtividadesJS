451f3a9f2a911fdb67e8081a9f8384ab98000e4b
Pedro-Lucas-Novaes <pedrolucas.novaes.dutra@gmail.com>
2025-09-10 21:31:08 -0300

    Add files via upload

--- DOM - Painel de controle de cores/JS/script.js
+++ DOM - Painel de controle de cores/JS/script.js
@@ -1,11 +1,33 @@
-function claro(){
-    document.querySelector(".container").style.backgroundColor = "white"
+function claro() {
+  document.querySelector(".container").style.backgroundColor = "white";
 }
 
-function reset(){
-    document.querySelector(".container").style.backgroundColor = "lightgray"
+function reset() {
+  document.querySelector(".container").style.backgroundColor = "lightgray";
 }
 
-function escuro(){
-    document.querySelector(".container").style.backgroundColor = "black"
+function escuro() {
+  document.querySelector(".container").style.backgroundColor = "black";
 }
+
+document.querySelector("#claro").addEventListener("mouseenter", () => {
+  document.querySelector(".claro").style.display = "none";
+  document.querySelector("#sol").style.display = "inline";
+});
+
+document.querySelector("#claro").addEventListener("mouseleave", () => {
+  document.querySelector(".claro").style.display = "inline";
+  document.querySelector("#sol").style.display = "none";
+});
+
+document.querySelector("#escuro").addEventListener("mouseenter", () => {
+  document.querySelector(".escuro").style.display = "none";
+  document.querySelector("#lua").style.display = "inline";
+});
+
+document.querySelector("#escuro").addEventListener("mouseleave", () => {
+  document.querySelector(".escuro").style.display = "inline";
+  document.querySelector("#lua").style.display = "none";
+});
+
+
