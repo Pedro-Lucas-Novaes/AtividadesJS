c7afd5bd70b87200cd51da7e5dfdbfdb54bcfb97
Pedro <pedrolucas.novaes.dutra@gmail.com>
2025-10-05 16:21:17 -0300

    att

--- ATV-NodeJS/controllers/FilmesController.js
+++ ATV-NodeJS/controllers/FilmesController.js
@@ -1,16 +1,36 @@
 // Importando o Express
-import express from "express"
+import express from "express";
 // Carregando na variável router o express.Router() que é responsavel por gerenciar as rotas da aplicação
-const router = express.Router()
+const router = express.Router();
 
 // ROTA DE CLIENTES
 router.get("/filmes", (req, res) => {
   const filmes = [
-    { nome: "Harry Potter e o Prisioneiro de Azkaban", cpf: "123.456.789-00", endereco: "Rua das Flores, 34", img: "/imgs/imgF001.png" },
-    { nome: "Vida de Inseto", cpf: "123.456.789-00", endereco: "Rua Diamante, 100", img: "/imgs/imgF002.png" },
-    { nome: "The Moon: Sobrevivente", cpf: "123.456.789-00", endereco: "Rua Ceará, 30", img: "/imgs/imgF003.png" },
-    { nome: "Cidade de Deus", cpf: "123.456.789-00", endereco: "Rua Curitiba, 22", img: "/imgs/imgF004.png" },
-    { nome: "Twisters", cpf: "123.456.789-00", endereco: "Rua Curitiba, 22", img: "/imgs/imgF005.png" },
+    {
+      nome: "Harry Potter e o Prisioneiro de Azkaban",
+      preco: 3000,
+      img: "/imgs/imgF001.png",
+    },
+    {
+      nome: "Vida de Inseto",
+      preco: 2000,
+      img: "/imgs/imgF002.png",
+    },
+    {
+      nome: "The Moon: Sobrevivente",
+      preco: 1500,
+      img: "/imgs/imgF003.png",
+    },
+    {
+      nome: "Cidade de Deus",
+      preco: 7000,
+      img: "/imgs/imgF004.png",
+    },
+    {
+      nome: "Twisters",
+      preco: 1000,
+      img: "/imgs/imgF005.png",
+    },
   ];
   res.render("filmes", {
     filmes: filmes,
--- ATV-NodeJS/controllers/SeriesController.js
+++ ATV-NodeJS/controllers/SeriesController.js
@@ -1,15 +1,35 @@
-import express from "express"
+import express from "express";
 
-const router = express.Router()
+const router = express.Router();
 
 // ROTA DE PEDIDOS
 router.get("/series", (req, res) => {
   const series = [
-    { nome: "Breaking Bad", produto: "abacaxi", valorDoPedido: "9,5", img: "/imgs/imgS001.png" },
-    { nome: "WandaVision", produto: "melão", valorDoPedido: "11,77", img: "/imgs/imgS002.png" },
-    { nome: "Lucifer", produto: "toranja", valorDoPedido: "37,98", img: "/imgs/imgS003.png" },
-    { nome: "The Witcher", produto: "laranja", valorDoPedido: "22,99", img: "/imgs/imgS004.png" },
-    { nome: "Wandinha", produto: "laranja", valorDoPedido: "22,99", img: "/imgs/imgS005.png" },
+    {
+      nome: "Breaking Bad",
+      preco: 2500,
+      img: "/imgs/imgS001.png",
+    },
+    {
+      nome: "WandaVision",
+      preco: 6800,
+      img: "/imgs/imgS002.png",
+    },
+    {
+      nome: "Lucifer",
+      preco: 9200,
+      img: "/imgs/imgS003.png",
+    },
+    {
+      nome: "The Witcher",
+      preco: 2300,
+      img: "/imgs/imgS004.png",
+    },
+    {
+      nome: "Wandinha",
+      preco: 3000,
+      img: "/imgs/imgS005.png",
+    },
   ];
   res.render("series", {
     series: series,
